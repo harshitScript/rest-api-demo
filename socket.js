77dddc915c229b226a7f4c19f5f4c7ab561763a9
harshitScript <85029089+harshitScript@users.noreply.github.com>
2022-10-31 00:14:59 +0530

    cors headers added to the web-socket server

--- socket.js
+++ socket.js
@@ -2,8 +2,24 @@ const io = require("socket.io");
 
 let socket;
 
+const handlePreflightRequest = (req, res) => {
+  res.writeHead(200, {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Credentials": true,
+  });
+
+  return res.end();
+};
+
 const connectSocket = (httpServer) => {
-  socket = io(httpServer);
+  socket = io(httpServer, {
+    cors: {
+      origins: ["*"],
+      handlePreflightRequest,
+    },
+  });
 
   return socket;
 };
